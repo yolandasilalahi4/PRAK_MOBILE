59a640678906e86cb4c3354a8572a4ab4fcebe74
yolandasilalahi4 <yolandasilalahi100299@gmail.com>
2020-01-19 15:17:46 +0700

    bebeb

---
+++ MyClassSE01A/app/src/main/java/com/example/myclass_se_01_a/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myclass_se_01_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button LihatData = findViewById(R.id.lihatbtn);
+        Button InputData = findViewById(R.id.inputbtn);
+        Button informasi = findViewById(R.id.infobtn);
+        LihatData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(MainActivity.this, LihatData.class);
+                startActivity(inte);
+            }
+        });
+        InputData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(MainActivity.this, InputData.class);
+                startActivity(inte);
+            }
+        });
+        informasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(MainActivity.this, Informasi.class);
+                startActivity(inte);
+            }
+        });
+    }
+}
